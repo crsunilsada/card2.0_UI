799abe42d5c766a617bf0644fd12a36981ac6ff7
rajavenkata muccharla <rajavenkata.muccharla@criticalriver.com>
2023-03-01 11:51:38 +0530

    Header Images Updated with conditions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,22 @@ import banner from 'images/Header1.png'
 
 const Header = () => {
   const router = useRouter();
+  let headerimage = {};
+  let headerclassname = {};
+  if (router.pathname === '/login') {
+    headerimage = '/images/Header1.png';
+    headerclassname = '';
+  }
+  else if (router.pathname === '/homePage') {
+    headerimage = '/images/homepageheader.png';
+    headerclassname = 'mainheader-space';
+  }
+  else {
+    headerimage = '/images/Header1.png';
+    headerclassname = 'header-space';
+  }
   return (
-    <><div className="fixed-top "><img src={"/images/Header1.png"} className="header-image"></img><header className={styles.header_body}>
+    <><div className={`mainHeader ${headerclassname}`}><div className={`fixed-top ${headerclassname}`}><img src={"/images/Header1.png"} className="header-image"></img><header className={styles.header_body}>
       <Navbar className="navigation bg-primary" bg="light" expand="lg">
         <Container className="headerNavContainer ">
           <div className="header-main-nav">
@@ -40,7 +54,7 @@ const Header = () => {
     </header>
     </div>
     <div className="header-space"></div>
-    </>
+    </div></>
   );
 };
 
