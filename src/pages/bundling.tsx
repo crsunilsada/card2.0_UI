f8df03d0aded9fd766db4c9cf5fcb9a222bb0b77
rajavenkata muccharla <rajavenkata.muccharla@criticalriver.com>
2023-03-24 13:24:22 +0530

    bundling responsive

--- src/pages/bundling.tsx
+++ src/pages/bundling.tsx
@@ -108,10 +108,10 @@ const Bundling = () => {
                                         <Col lg={4} md={4} xs={12} className="pageNextBtn">
                                             <Row className="mx-4">
                                                 <Col lg={6} md={12} xs={12} className="mb-3">
-                                                    <button type="button" className="next p-2 m-1"onClick={handleShow}>Start Bundling</button>
+                                                    <button type="button" className="bluebuttonclass p-2 m-1"onClick={handleShow}>Start Bundling</button>
                                                 </Col>
-                                                <Col lg={4} md={12} xs={12} className="">
-                                                    <button type="button" className="clear m-1"> Clear</button>
+                                                <Col lg={4} md={12} xs={12}>
+                                                    <button type="button" className="whitebuttonclass"> Clear</button>
                                                 </Col>
                                             </Row>
                                         </Col>
